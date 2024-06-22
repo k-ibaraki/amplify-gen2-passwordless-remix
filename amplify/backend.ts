88828b14e738f862fb67149d605e22fdeef5a4a0
k-ibaraki <ibaraki@ncdc.co.jp>
2024-06-22 17:57:05 +0900

    update backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,9 +1,45 @@
+import { Stack } from "aws-cdk-lib/core"
+import * as cdk from 'aws-cdk-lib';
+import { Passwordless } from "amazon-cognito-passwordless-auth/cdk";
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
-defineBackend({
+// 環境変数を取得
+const AUTH_FRONTEND_URL = process.env.AUTH_FRONTEND_URL ?? "";
+const AUTH_FRONTEND_HOST = process.env.AUTH_FRONTEND_HOST ?? "";
+const AUTH_EMAIL_FROM_ADDRESS = process.env.AUTH_EMAIL_FROM_ADDRESS ?? "";
+
+// Amplifyのバックエンドを定義
+const backend = defineBackend({
   auth,
+});
+
+// ユーザープールとクライアントを取得
+const userPool = backend.auth.resources.userPool as cdk.aws_cognito.UserPool;
+const userPoolClient = backend.auth.resources.userPoolClient as cdk.aws_cognito.UserPoolClient;
+// AuthのCDK Stackを取得
+const authStack = Stack.of(userPool);
+
+// Amplifyが作ったuserpoolにパスワードレス認証フローを追加
+const passwordless = new Passwordless(authStack, "Passwordless", {
+  userPool,
+  userPoolClients: [userPoolClient],
+  allowedOrigins: [
+    AUTH_FRONTEND_URL!
+  ],
+  fido2: {
+    allowedRelyingPartyIds: [
+      AUTH_FRONTEND_HOST!
+    ],
+  },
+  magicLink: {
+    sesFromAddress: AUTH_EMAIL_FROM_ADDRESS!,
+  },
+});
+
+// configurationファイルにFIDO2のURLを出力
+backend.addOutput({
+  custom: {
+    fido2ApiUrl: passwordless.fido2Api?.url ?? "",
+  },
 });
