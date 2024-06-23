7bf8b9d79201ae1b1b3176ed438d68aa255a8e96
k-ibaraki <ibaraki@ncdc.co.jp>
2024-06-23 16:11:01 +0900

    fix frontend

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,45 +1,35 @@
 import type { MetaFunction } from "@remix-run/node";
 import { usePasswordless } from "amazon-cognito-passwordless-auth/react";
 
-// LogoutButton
-function LogoutButton() {
-  const { signOut } = usePasswordless();
+// Fido2CredentialsComponent
+function Fido2Credentials() {
+  const { fido2Credentials, signOut, tokens } = usePasswordless();
   const handleLogout = async () => {
     await signOut();
     window.location.href = '/';
   };
   return (
     <div>
-      <button onClick={handleLogout}>
-        ログアウト
-      </button>
-    </div>
-  );
-}
-
-// DeleteFido2CredentialsComponent
-function DeleteFido2Credentials() {
-  const { fido2Credentials, signOut } = usePasswordless();
-  const handleLogout = async () => {
-    await signOut();
-    window.location.href = '/';
-  };
-  return (
-    <div>
-      <div>
-        <ul>
-        {fido2Credentials?.map((c) => (
-          <li>
-            <button onClick={() => c.delete()}>Delete PassKey : {c.friendlyName}</button>
-          </li>
-        )) ?? <></>}
-        </ul>
-      </div>
       <div>
         <button onClick={handleLogout}>
           Logout
         </button>
       </div>
+      <div>------
+        <ul>
+        {fido2Credentials?.map((c) => (
+          <li>
+            <button onClick={() => c.delete()}>Delete PassKey[{c.friendlyName}]</button>
+          </li>
+        ))}
+        </ul>
+      </div>
+      <div>------
+        <div>accessToken: {tokens?.accessToken}</div>
+        <div>idToken: {tokens?.idToken}</div>
+        <div>refreshToken: {tokens?.refreshToken}</div>
+      </div>
+
     </div>
   );
   }
@@ -54,30 +44,8 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix (SPA Mode)</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/guides/spa-mode"
-            rel="noreferrer"
-          >
-            SPA Mode Guide
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-      <DeleteFido2Credentials />
+      <h1 className="text-3xl">Passkey Sample</h1>
+      <Fido2Credentials />
     </div>    
   );
 }
